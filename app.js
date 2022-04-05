b0c08292f9cacc16a60cda72dc88340599ef2e8c
Ayman <aym.omran@outlook.com>
2022-04-05 12:18:40 +0200

    v 6 Route Parameters

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT||3000;
+const Students = [
+    {name: 'Ali', dept: 'PD', id: 1},
+    {name: 'Nour', dept: 'SA', id: 2},
+    {name: 'Mona', dept: 'MD', id: 3},
+    {name: 'sara', dept: 'SAP', id: 4},
+    {name: 'Mostafa', dept: 'EB', id: 5},
+    {name: 'Ahmed', dept: 'GD', id: 1},
+    {name: 'Noha', dept: 'GA', id: 2},
+];
+
 
 app.get('/',(req, res) =>  {
     console.log("request riseved");
     res.send("Hi...");
 });
+
+//Request all students
+app.get("/api/Students",(req,res)=>{
+    res.json(Students);
+});
+
+//Request student by id
+app.get("/api/Students/:id",(req,res)=>{
+    let id = req.params.id;
+    const std = Students.find((val, idx, arr)=> {
+        return val.id == id;
+    })
+
+    if(std){
+        res.json(std);
+    }else{
+        res.send("Not Found");
+    } 
+});
+
+
+
+
 
 app.listen(port, () => {
     console.log(`app listining on port ${port}`);
